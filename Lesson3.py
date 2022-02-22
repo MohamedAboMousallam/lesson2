f181cfbe8854d2014f605aa36c472b92fdb4e39f
MohamedAboMousallam <86533055+MohamedAboMousallam@users.noreply.github.com>
2022-02-22 14:48:09 +0100

    conflict 2

--- Lesson3.py
+++ Lesson3.py
@@ -3,3 +3,15 @@ from cgi import print_environ
 print("Deleted the lines")
 for i in range(8):
     print("work?")
+print("CONFLICT")
+print("CONFLICT")
+print("CONFLICT")
+print("CONFLICT")
+print("CONFLICT")
+print("CONFLICT")
+print("CONFLICT")
+
+print("Conflict 2")
+print("Conflict 2")
+print("Conflict 2")
+print("Conflict 2")
